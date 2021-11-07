be66f22fe9b4562dee16d8108c58fb8413d798a5
MH <86976920+mehelin@users.noreply.github.com>
2021-11-07 20:26:24 +0200

    Add files via upload

---
+++ controllers/notes.js
@@ -0,0 +1,51 @@
+const notesRouter = require('express').Router()
+const Note = require('../models/note')
+
+notesRouter.get('/', async (request, response) => { 
+  const notes = await Note.find({})
+  response.json(notes.map(note => note.toJSON()))
+})
+
+notesRouter.post('/', async (request, response) => {
+  const body = request.body
+  
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  })
+  
+  const savedNote = await note.save()
+  response.json(savedNote.toJSON())
+})
+  
+notesRouter.get('/:id', async (request, response) => {
+  const note = await Note.findById(request.params.id)
+  if (note) {
+    response.json(note.toJSON())
+  } else {
+    response.status(404).end()
+  }
+})
+
+notesRouter.delete('/:id', async (request, response) => {
+  await Note.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+notesRouter.put('/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote.toJSON())
+    })
+    .catch(error => next(error))
+})
+
+module.exports = notesRouter
