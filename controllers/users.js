a89a6323b62dd15a7cacb189dc4b4ebaf7617643
MH <86976920+mehelin@users.noreply.github.com>
2021-11-08 16:30:15 +0200

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -2,28 +2,40 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({}).populate('blogs', {
+    url: 1,
+    title: 1,
+    author: 1,
+  })
+  response.json(users)
+})
+
+usersRouter.delete('/:id', async (request, response) => {
+  await User.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
 usersRouter.post('/', async (request, response) => {
   const body = request.body
 
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(body.password, saltRounds)
+  if (request.body.password.length < 3) {
+    response.status(404).json({
+      error: 'password must be at least 3 characters long',
+    })
+  } else {
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
-  const user = new User({
-    username: body.username,
-    name: body.name,
-    passwordHash,
-  })
+    const user = new User({
+      username: body.username,
+      name: body.name,
+      passwordHash,
+    })
 
-  const savedUser = await user.save()
-
-  response.json(savedUser)
-})
-
-usersRouter.get('/', async (request, response) => {
-  const users = await User
-    .find({}).populate('notes', { content: 1, date: 1 })
-    
-  response.json(users.map(u => u.toJSON()))
+    const savedUser = await user.save()
+    response.json(savedUser)
+  }
 })
 
 module.exports = usersRouter
