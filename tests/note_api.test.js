09de0dca0c9dd1ebb7970f950d86335b880f4aa4
MH <86976920+mehelin@users.noreply.github.com>
2021-11-08 02:20:30 +0200

    Update note_api.test.js

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -3,17 +3,15 @@ const mongoose = require('mongoose')
 const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
-
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
 const Note = require('../models/note')
 
 describe('when there is initially some notes saved', () => {
+  
   beforeEach(async () => {
     await Note.deleteMany({})
-
-    const noteObjects = helper.initialNotes
-      .map(note => new Note(note))
-    const promiseArray = noteObjects.map(note => note.save())
-    await Promise.all(promiseArray)
+    await Note.insertMany(helper.initialNotes)
   })
 
   test('notes are returned as json', async () => {
@@ -26,7 +24,7 @@ describe('when there is initially some notes saved', () => {
   test('all notes are returned', async () => {
     const response = await api.get('/api/notes')
 
-    expect(response.body.length).toBe(helper.initialNotes.length)
+    expect(response.body).toHaveLength(helper.initialNotes.length)
   })
 
   test('a specific note is within the returned notes', async () => {
@@ -49,14 +47,14 @@ describe('when there is initially some notes saved', () => {
         .get(`/api/notes/${noteToView.id}`)
         .expect(200)
         .expect('Content-Type', /application\/json/)
+      
+      const processedNoteToView = JSON.parse(JSON.stringify(noteToView))
 
-      expect(resultNote.body).toEqual(noteToView)
+      expect(resultNote.body).toEqual(processedNoteToView)
     })
 
     test('fails with statuscode 404 if note does not exist', async () => {
       const validNonexistingId = await helper.nonExistingId()
-
-      console.log(validNonexistingId)
 
       await api
         .get(`/api/notes/${validNonexistingId}`)
@@ -73,6 +71,7 @@ describe('when there is initially some notes saved', () => {
   })
 
   describe('addition of a new note', () => {
+
     test('succeeds with valid data', async () => {
       const newNote = {
         content: 'async/await simplifies making async calls',
@@ -87,7 +86,7 @@ describe('when there is initially some notes saved', () => {
 
 
       const notesAtEnd = await helper.notesInDb()
-      expect(notesAtEnd.length).toBe(helper.initialNotes.length + 1)
+      expect(notesAtEnd).toHaveLength(helper.initialNotes.length + 1)
 
       const contents = notesAtEnd.map(n => n.content)
       expect(contents).toContain(
@@ -107,11 +106,12 @@ describe('when there is initially some notes saved', () => {
 
       const notesAtEnd = await helper.notesInDb()
 
-      expect(notesAtEnd.length).toBe(helper.initialNotes.length)
+      expect(notesAtEnd).toHaveLength(helper.initialNotes.length)
     })
   })
 
   describe('deletion of a note', () => {
+
     test('succeeds with status code 204 if id is valid', async () => {
       const notesAtStart = await helper.notesInDb()
       const noteToDelete = notesAtStart[0]
@@ -122,7 +122,7 @@ describe('when there is initially some notes saved', () => {
 
       const notesAtEnd = await helper.notesInDb()
 
-      expect(notesAtEnd.length).toBe(
+      expect(notesAtEnd).toHaveLength(
         helper.initialNotes.length - 1
       )
 
@@ -131,6 +131,60 @@ describe('when there is initially some notes saved', () => {
       expect(contents).not.toContain(noteToDelete.content)
     })
   })
+})
+
+describe('when there is initially one user at db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('`username` to be unique')
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
+  })  
 })
 
 afterAll(() => {
