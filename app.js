e6f7fb7d321eed53835ff3e049984952bc52f2b3
MH <86976920+mehelin@users.noreply.github.com>
2021-11-10 19:24:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const config = require('./utils/config')
-const express = require('express')
-require('express-async-errors')
-const app = express()
-const cors = require('cors')
+const config = require('./utils/config');
+const express = require('express');
+require('express-async-errors');
+const app = express();
+const cors = require('cors');
 
-const blogsRouter = require('./controllers/blogs')
-const usersRouter = require('./controllers/users')
-const loginRouter = require('./controllers/login')
+const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 
-const middleware = require('./utils/middleware')
-const logger = require('./utils/logger')
-const mongoose = require('mongoose')
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
+const mongoose = require('mongoose');
 
-logger.info('connecting to', config.MONGODB_URI)
+logger.info('connecting to', config.MONGODB_URI);
 
 mongoose
   .connect(config.MONGODB_URI, {
@@ -22,37 +22,39 @@ mongoose
     useCreateIndex: true,
   })
   .then(() => {
-    logger.info('connected to MongoDB')
+    logger.info('connected to MongoDB');
   })
   .catch((error) => {
-    logger.error('error connecting to MongoDB:', error.message)
-  })
+    logger.error('error connecting to MongoDB:', error.message);
+  });
 
 app.use((req, res, next) => {
   if (req.originalUrl && req.originalUrl.split('/').pop() === 'favicon.ico') {
-    return res.sendStatus(204)
+    return res.sendStatus(204);
   }
-  return next()
-})
+  return next();
+});
 
-app.use(cors())
-app.use(express.static('build'))
-app.use(express.json())
-app.use(middleware.morganLogger())
+app.use(cors());
+app.use(express.static('build'));
+app.use(express.json());
+app.use(middleware.morganLogger());
 
-app.use(middleware.tokenExtractor)
+app.use(middleware.tokenExtractor);
 
-app.use('/api/blogs', blogsRouter)
-app.use('/api/users', usersRouter)
-app.use('/api/login', loginRouter)
+app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 if (process.env.NODE_ENV === 'test') {
-  const testingRouter = require('./controllers/testing')
-  app.use('/api/testing', testingRouter)
+  const testingRouter = require('./controllers/testing');
+  app.use('/api/testing', testingRouter);
 }
 
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
-module.exports = app
+module.exports = app;
+
+
 
