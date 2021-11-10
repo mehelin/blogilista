e4f644048012267ba8321f7f45d37e5322e0286c
MH <86976920+mehelin@users.noreply.github.com>
2021-11-10 19:22:06 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,8 +24,12 @@ module.exports = {
     ],
     'semi': [
       'error',
-      'never'
-    ],
+      'always'
+      ],
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
     'no-console': 0,
-  }
+  },
 }
