172748baf6f24baf26bdd36d24221f4ab39aea97
MH <86976920+mehelin@users.noreply.github.com>
2021-11-12 02:19:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const app = require('./app') // varsinainen Express-sovellus
-const http = require('http')
-const config = require('./utils/config')
-const logger = require('./utils/logger')
+const app = require('./app'); // varsinainen Express-sovellus
+const http = require('http');
+const config = require('./utils/config'); // sovelluksen muut osat pääsevät y.muuttujiin
+const logger = require('./utils/logger');
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 server.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
-})
+  logger.info(`Server running on port ${config.PORT}`); // sovelluksen muut osat pääsevät y.muuttujiin
+});
